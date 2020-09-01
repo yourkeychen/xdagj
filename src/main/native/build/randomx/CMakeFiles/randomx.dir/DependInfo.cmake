ca18e56d7c29f66a1a839fb2bd9cd70d94bef2aa
Bill Levesque <amazingbill2017@gmail.com>
2020-09-01 20:48:36 +0800

    1.port randomx to jni

---
+++ src/main/native/build/randomx/CMakeFiles/randomx.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/argon2_avx2.c" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/argon2_avx2.c.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/argon2_core.c" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/argon2_core.c.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/argon2_ref.c" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/argon2_ref.c.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/argon2_ssse3.c" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/argon2_ssse3.c.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/blake2/blake2b.c" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/blake2/blake2b.c.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/jit_compiler_x86_static.S" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/jit_compiler_x86_static.S.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/reciprocal.c" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/reciprocal.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "randomx_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../randomx/../jni"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/aes_hash.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/aes_hash.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/allocator.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/allocator.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/assembly_generator_x86.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/assembly_generator_x86.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/blake2_generator.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/blake2_generator.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/bytecode_machine.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/bytecode_machine.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/cpu.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/cpu.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/dataset.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/dataset.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/instruction.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/instruction.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/instructions_portable.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/instructions_portable.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/jit_compiler_x86.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/jit_compiler_x86.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/randomx.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/randomx.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/randomx_jni.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/randomx_jni.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/soft_aes.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/soft_aes.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/superscalar.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/superscalar.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/virtual_machine.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/virtual_machine.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/virtual_memory.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/virtual_memory.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/vm_compiled.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/vm_compiled.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/vm_compiled_light.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/vm_compiled_light.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/vm_interpreted.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/vm_interpreted.cpp.o"
+  "/Users/mac/IdeaProjects/github/xdagj/src/main/native/randomx/src/vm_interpreted_light.cpp" "/Users/mac/IdeaProjects/github/xdagj/src/main/native/build/randomx/CMakeFiles/randomx.dir/src/vm_interpreted_light.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "randomx_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../randomx/../jni"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
